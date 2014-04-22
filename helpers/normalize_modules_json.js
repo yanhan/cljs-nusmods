97766b3f7d1b80e84f3e8c2700d4bc6098583e13
Pang Yan Han <pangyanhan@gmail.com>
2014-04-22 08:45:31 +0800

    Refactor out code for normalizing department strings
    This is to prepare for upcoming patches which will create a facet for filtering
    by faculty and department.
    
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- helpers/normalize_modules_json.js
+++ helpers/normalize_modules_json.js
@@ -3,6 +3,7 @@
 var fs = require("fs");
 var _ = require("lodash");
 var SharedGlobals = require("./shared_globals.js");
+var NormalizeDepartment = require("./normalize_department.js");
 var MODULES_ARRAY = require(__dirname + "/../modules.json");
 
 var STRING_KEYS = [
@@ -62,45 +63,12 @@ var ARRAY_OF_STRINGS_KEYS = [
         }
       }
     }
-    // Department - we convert the strings to title case.
-    // For instance, "COMPUTER SCIENCE" is converted to "Computer Science".
-    //
-    // Special Cases:
-    //   "CTR FOR ENGLISH LANGUAGE COMMUNICATION"
-    //     --> Center for English Language Communication
-    //   "DUKE-NUS GRADUATE MEDICAL SCHOOL S'PORE"
-    //     --> Duke NUS Graduate Medical School Singapore
-    //   "NURSING/ALICE LEE CTR FOR NURSING STUD"
-    //     --> Nursing / Alice Lee Center for Nursing Studies
-    //   "NUS GRAD SCH FOR INTEGRATIVE SCI & ENGG"
-    //     --> NUS Graduate School for Integrative Sciences and Engineering
-    //   "SINGAPORE-MIT ALLIANCE"
-    //     --> Singapore-MIT Alliance
-    //   "YALE-NUS COLLEGE"
-    //     --> Yale-NUS College
+    // normalize department string
     if (_.has(mod, "Department")) {
-      department = mod.Department;
-      if (department === "CTR FOR ENGLISH LANGUAGE COMMUNICATION") {
-        department = "Center for English Language Communication";
-      } else if (department === "DUKE-NUS GRADUATE MEDICAL SCHOOL S'PORE") {
-        department = "Duke-NUS Graduate Medical School Singapore";
-      } else if (department === "NURSING/ALICE LEE CTR FOR NURSING STUD") {
-        department = "Nursing / Alice Lee Center for Nursing Studies";
-      } else if (department === "NUS GRAD SCH FOR INTEGRATIVE SCI & ENGG") {
-        department = "NUS Graduate School for Integrative Sciences and Engineering";
-      } else if (department === "SINGAPORE-MIT ALLIANCE") {
-        department = "Singapore-MIT Alliance";
-      } else if (department === "YALE-NUS COLLEGE") {
-        department = "Yale-NUS College";
-      } else {
-        department = department.replace(/\w\S*/g, function(txt) {
-          txt = txt.trim();
-          return txt.charAt(0).toUpperCase() + txt.slice(1).toLowerCase();
-        });
-      }
-      mod.Department = department;
+      mod.Department = NormalizeDepartment.normalize_department_string(
+        mod.Department
+      );
     }
-
     // convert `ModuleCredit` to integer
     if (_.has(mod, "ModuleCredit")) {
       mod.ModuleCredit = _.parseInt(mod.ModuleCredit);
