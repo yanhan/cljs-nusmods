240451bc3a0d784bbcba6b455ec1c253c6db4411
Pang Yan Han <pangyanhan@gmail.com>
2014-04-23 21:58:32 +0800

    key_frequency - inspect frequency of keys in lecture objects
    Signed-off-by: Pang Yan Han <pangyanhan@gmail.com>

--- inspect_scripts/key_frequency.js
+++ inspect_scripts/key_frequency.js
@@ -27,4 +27,37 @@ var MODULE_KEYS = [
   _.forEach(moduleKeyFreqs, function(freq, key) {
     console.log("modules with key \"" + key + "\" = " + freq);
   });
+
+  var arrayOfModulesWithTimetable =
+    _.filter(MODULES_ARRAY, function(module) {
+      return _.has(module, "Timetable");
+    });
+  var timetableKeyFreqs =
+    arrayOfModulesWithTimetable
+      .reduce(function(keyFreqHash, module) {
+        _.forEach(module.Timetable, function(lessonObject) {
+          _(lessonObject).keys().forEach(function(key) {
+            if (!_.has(keyFreqHash, key)) {
+              keyFreqHash[key] = 0;
+            }
+            keyFreqHash[key] += 1;
+          });
+        });
+        return keyFreqHash;
+      }, {});
+  console.log(
+    "\nThere are " + arrayOfModulesWithTimetable.length +
+    " modules with the \"Timetable\" key and a total of " +
+    _(arrayOfModulesWithTimetable)
+      .map(function(mod) {
+        return mod.Timetable;
+      })
+      .flatten()
+      .value()
+      .length +
+    " lesson objects."
+  );
+  _.forEach(timetableKeyFreqs, function(freq, key) {
+    console.log("  " + freq + " lesson objects have the key \"" + key +  "\"");
+  });
 })();
